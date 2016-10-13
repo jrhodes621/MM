b096e676db50ef5aa889a3f1e7bc3174c854a043
James Rhodes <james@somehero.com>
2016-10-13 11:10:04 -0400

    add me endpoint

---
+++ api/private/me.js
@@ -0,0 +1,12 @@
+var express    = require('express');        // call express
+var router = express.Router();              // get an instance of the express Router
+var mongoose   = require('mongoose');
+
+// on routes that end in /me
+// ----------------------------------------------------
+router.route('')
+  .get(function(req, res) {
+    res.json(req.user);
+  });
+
+module.exports = router;
