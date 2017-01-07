30fa16fb4a3dc0440c095156979049b748bc5a6a
James Rhodes <james@somehero.com>
2017-01-07 16:23:47 -0500

    fix up reference name

--- helpers/stripe_event_processors/customer_processor.js
+++ helpers/stripe_event_processors/customer_processor.js
@@ -1,5 +1,5 @@
 var ActivityHelper = require('../../helpers/activity_helper');
-var CustomerHelper = require('../../helpers/customer_helper');
+var CustomerHelper = require('../../helpers/customer_parser');
 var MembershipHelper = require('../../helpers/membership_helper');
 var PushNotificationHelper = require('../../helpers/push_notification_helper');
 var SubscriptionHelper = require('../../helpers/subscription_helper');
