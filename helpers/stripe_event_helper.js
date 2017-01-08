9ea12a5cec934c511a0d1b345fcb79ce060d4f55
James Rhodes <james@somehero.com>
2017-01-07 22:09:49 -0500

    fix reference issues

--- helpers/stripe_event_helper.js
+++ helpers/stripe_event_helper.js
@@ -1,10 +1,12 @@
 var async = require("async");
-var ActivityHelper =
-var PushNotificationHelper =
-var User
+var ActivityHelper = require('./helpers/activity_helper');
+var PushNotificationHelper = require('./helpers/push_notification_helper');
+var User = require('../models/user');
+
 module.exports = {
   notifyUsers: function(notification_type, bull, calf, plan, message_bull, message_calf, source, received_at, callback) {
-    var payload;
+    var payload = {'messageFrom': 'MemberMoose',
+                  'type': "customer_created"};
 
     var activities = [];
     User.find({ "account": bull }, function(err, users) {
