0a8d150cad2be01d7db1756a58266b518ef26a63
James Rhodes <james@somehero.com>
2017-01-08 20:43:59 -0500

    implement charge processor

--- helpers/charge_helper.js
+++ helpers/charge_helper.js
@@ -9,12 +9,58 @@ function getPaymentCardForCharge(charge, reference_id, callback) {
     });
 }
 module.exports = {
-  getCharge: function(charge_id, callback) {
-    Subscription.findOne({ "reference_id": charge_id })
-    .populate('user')
+  getCharge: function(charge_id, membership, callback) {
+    Charge.findOne({ "reference_id": charge_id, "membership": membership })
+    .populate('membership')
+    .populate('payment_card')
     .exec(function(err, charge) {
       callback(err, charge)
     })
+  },
+  parseChargeFromStripe: function(charge, membership, bull, stripe_charge, callback) {
+    async.waterfall(
+      function getPaymentCard(callback) {
+        PaymentCard.findOne({"reference_id": stripe_charge.source.id}, function(err, payment_card) {
+          if(!payment_card) { return callback(new Error("Payment Card not found"), null); }
+
+          callback(err, payment_card);
+        });
+      },
+      function parseCharge(payment_card, callback) {
+        if(!charge) {
+          charge = new Charge();
+        }
+        charge.reference_id = stripe_charge.id;
+        charge.amount = stripe_charge.amount/100.0;
+        charge.amount_refunded = stripe_charge.amount_refunded;
+        charge.balance_transaction = stripe_charge.balance_transaction;
+        charge.captured = stripe_charge.captured;
+        charge.charge_created = new Date(stripe_charge.created * 1000);
+        charge.currency = stripe_charge.currency;
+        charge.description = stripe_charge.description;
+        charge.destination = stripe_charge.destination;
+        charge.dispute = stripe_charge.dispute;
+        charge.failure_code = stripe_charge.failure_code;
+        charge.failure_message = stripe_charge.failure_message;
+        charge.invoice = stripe_charge.invoice;
+        charge.paid = stripe_charge.paid;
+        charge.receipt_email = stripe_charge.receipt_email;
+        charge.receipt_number = stripe_charge.receipt_number;
+        charge.refunded = stripe_charge.refunded;
+        charge.shipping = stripe_charge.shipping;
+        charge.source_transfer = stripe_charge.source_transfer;
+        charge.statement_descriptor = stripe_charge.statement_descriptor;
+        charge.status = stripe_charge.status;
+        charge.membership = membership._id;
+        charge.payment_card = payment_card;
+
+        charge.save(function(err) {
+          callback(err, charge);
+        });
+      }
+    ], function(err) {
+      callback(err, charge);
+    });
   },
   parse: function(user, stripe_charges, membership, callback) {
     var all_charges = [];
--- helpers/stripe_event_processors/charge_processor.js
+++ helpers/stripe_event_processors/charge_processor.js
@@ -5,99 +5,158 @@ var SubscriptionHelper = require('../../helpers/subscription_helper');
 var StripeManager = require('../stripe_manager');
 
 module.exports = {
-  processFailed: function(stripe_event, bull, callback) {
-    // Do something with event_json
-    //var event_types = ['charge.failed', 'charge.refunded', 'charge.succeeded']
-    var reference_id = stripe_event.raw_object.data.object.customer;
-    var invoice_id = stripe_event.raw_object.data.object.invoice;
-    var source = "Stripe";
-    var received_at = received_at = new Date(stripe_event.raw_object.created*1000);
+  processCaptured: function(stripe_event, bull, callback) {
+    let stripe_charge = stripe_event.raw_object.data.object;
+    let charge_id = stripe_charge.id;
+    let customer_id = stripe_charge.customer;
+    let source = "Stripe";
+    let received_at = new Date(stripe_event.raw_object.created*1000);
+    let payment_total = stripe_charge.amount;
 
-    MembershipHelper.getMembershipByReference(reference_id, function(err, membership) {
+    let opts = { format: '%s%v %c', code: 'USD', symbol: '$' }
+    let payment_total_formatted = FormatCurrency(payment_total, opts)
+
+    MembershipHelper.getMembershipByReference(customer_id, function(err, membership) {
       if(err) { return callback(err, null); }
       if(!membership) { return callback(new Error("Calf not found"), null) }
 
-      var stripe_api_key = membership.account.stripe_connect.access_token;
+      ChargeHelper.getCharge(charge_id, function(err, charge) {
+        if(err) { return callback(err, null); }
 
-      //find invoice
-      StripeManager.getInvoice(stripe_api_key, invoice_id, function(err, stripe_invoice) {
-        var subscription_id = stripe_invoice.subscription;
+        ChargeHelper.parseChargeFromStripe(charge, membership, bull, stripe_charge, function(err, charge) {
+          if(err) { return callback(err, charge); }
 
-        SubscriptionHelper.getSubscription(subscription_id, function(err, subscription) {
-          if(err) { return callback(err, null); }
-          if(!subscription) { return callback(new Error("Subscription not found"), null) }
+          var message_calf = "Your charge of " + payment_total_formatted + " was captured.";
+          var message_bull= "A charge of " + payment_total_formatted + " from " + membership.user.email_address + " was captured.";
 
-          var message_calf = "Your payment of $4.00 for " + subscription.plan.name + " failed.";
-          var message_bull= "A payment of $4.00 for " + subscription.plan.name + " from " + membership.user.email_address + " failed.";
+          StripeEventHelper.notifyUsers("charge_captured", bull, membership.user, null, message_bull, message_calf, source, received_at, function(err, activities) {
+            callback(err, user);
+          });
+        })
+      });
+    });
+  },
+  processFailed: function(stripe_event, bull, callback) {
+    let stripe_charge = stripe_event.raw_object.data.object;
+    let charge_id = stripe_charge.id;
+    let customer_id = stripe_charge.customer;
+    let source = "Stripe";
+    let received_at = new Date(stripe_event.raw_object.created*1000);
+    let payment_total = stripe_charge.amount;
+
+    let opts = { format: '%s%v %c', code: 'USD', symbol: '$' }
+    let payment_total_formatted = FormatCurrency(payment_total, opts)
+
+    MembershipHelper.getMembershipByReference(customer_id, function(err, membership) {
+      if(err) { return callback(err, null); }
+      if(!membership) { return callback(new Error("Calf not found"), null) }
+
+      ChargeHelper.getCharge(charge_id, function(err, charge) {
+        if(err) { return callback(err, null); }
+
+        ChargeHelper.parseChargeFromStripe(charge, membership, bull, stripe_charge, function(err, charge) {
+          if(err) { return callback(err, charge); }
+
+          var message_calf = "Your charge of " + payment_total_formatted + " failed.";
+          var message_bull= "A charge of " + payment_total_formatted + " from " + membership.user.email_address + " failed.";
 
           StripeEventHelper.notifyUsers("charge_failed", bull, membership.user, null, message_bull, message_calf, source, received_at, function(err, activities) {
             callback(err, user);
           });
-        });
+        })
+      });
+    });
+  },
+  processPending: function(stripe_event, bull, callback) {
+    let stripe_charge = stripe_event.raw_object.data.object;
+    let charge_id = stripe_charge.id;
+    let customer_id = stripe_charge.customer;
+    let source = "Stripe";
+    let received_at = new Date(stripe_event.raw_object.created*1000);
+    let payment_total = stripe_charge.amount;
+
+    let opts = { format: '%s%v %c', code: 'USD', symbol: '$' }
+    let payment_total_formatted = FormatCurrency(payment_total, opts)
+
+    MembershipHelper.getMembershipByReference(customer_id, function(err, membership) {
+      if(err) { return callback(err, null); }
+      if(!membership) { return callback(new Error("Calf not found"), null) }
+
+      ChargeHelper.getCharge(charge_id, function(err, charge) {
+        if(err) { return callback(err, null); }
+
+        ChargeHelper.parseChargeFromStripe(charge, membership, bull, stripe_charge, function(err, charge) {
+          if(err) { return callback(err, charge); }
+
+          var message_calf = "Your charge of " + payment_total_formatted + " is pending.";
+          var message_bull= "A charge of " + payment_total_formatted + " from " + membership.user.email_address + " is pending.";
+
+          StripeEventHelper.notifyUsers("charge_pending", bull, membership.user, null, message_bull, message_calf, source, received_at, function(err, activities) {
+            callback(err, user);
+          });
+        })
       });
     });
   },
   processRefunded: function(stripe_event, bull, callback) {
-    // Do something with event_json
-    //var event_types = ['charge.failed', 'charge.refunded', 'charge.succeeded']
-    var reference_id = stripe_event.raw_object.data.object.customer;
-    var invoice_id = stripe_event.raw_object.data.object.invoice;
-    var source = "Stripe";
-    var received_at = received_at = new Date(stripe_event.raw_object.created*1000);
+    let stripe_charge = stripe_event.raw_object.data.object;
+    let charge_id = stripe_charge.id;
+    let customer_id = stripe_charge.customer;
+    let source = "Stripe";
+    let received_at = new Date(stripe_event.raw_object.created*1000);
+    let payment_total = stripe_charge.amount;
 
-    MembershipHelper.getMembershipByReference(reference_id, function(err, membership) {
+    let opts = { format: '%s%v %c', code: 'USD', symbol: '$' }
+    let payment_total_formatted = FormatCurrency(payment_total, opts)
+
+    MembershipHelper.getMembershipByReference(customer_id, function(err, membership) {
       if(err) { return callback(err, null); }
       if(!membership) { return callback(new Error("Calf not found"), null) }
 
-      var stripe_api_key = membership.account.stripe_connect.access_token;
+      ChargeHelper.getCharge(charge_id, function(err, charge) {
+        if(err) { return callback(err, null); }
 
-      //find invoice
-      StripeManager.getInvoice(stripe_api_key, invoice_id, function(err, stripe_invoice) {
-        var subscription_id = stripe_invoice.subscription;
+        ChargeHelper.parseChargeFromStripe(charge, membership, bull, stripe_charge, function(err, charge) {
+          if(err) { return callback(err, charge); }
 
-        SubscriptionHelper.getSubscription(subscription_id, function(err, subscription) {
-          if(err) { return callback(err, null); }
-          if(!subscription) { return callback(new Error("Subscription not found"), null) }
+          var message_calf = "Your charge of " + payment_total_formatted + " was refunded.";
+          var message_bull= "A charge of " + payment_total_formatted + " from " + membership.user.email_address + " was refunded.";
 
-          var message_calf = "Your payment of $4.00 for " + subscription.plan.name + " was refunded.";
-          var message_bull= "A payment of $4.00 for " + subscription.plan.name + " from " + membership.user.email + " was refuned.";
-
-          StripeEventHelper.notifyUsers("charge_refunded", bull, membership.user, subscription.plan, message_bull, message_calf, source, received_at, function(err, activities) {
+          StripeEventHelper.notifyUsers("charge_refunded", bull, membership.user, null, message_bull, message_calf, source, received_at, function(err, activities) {
             callback(err, user);
           });
-        });
+        })
       });
     });
   },
-  processSucceeded: function(stripe_event, bull, callback) {
-    // Do something with event_json
-    //var event_types = ['charge.failed', 'charge.refunded', 'charge.succeeded']
-    var reference_id = stripe_event.raw_object.data.object.customer;
-    var invoice_id = stripe_event.raw_object.data.object.invoice;
-    var source = "Stripe";
-    var received_at = received_at = new Date(stripe_event.raw_object.created*1000);
+  processUpdated: function(stripe_event, bull, callback) {
+    let stripe_charge = stripe_event.raw_object.data.object;
+    let charge_id = stripe_charge.id;
+    let customer_id = stripe_charge.customer;
+    let source = "Stripe";
+    let received_at = new Date(stripe_event.raw_object.created*1000);
+    let payment_total = stripe_charge.amount;
 
-    MembershipHelper.getMembershipByReference(reference_id, function(err, membership) {
+    let opts = { format: '%s%v %c', code: 'USD', symbol: '$' }
+    let payment_total_formatted = FormatCurrency(payment_total, opts)
+
+    MembershipHelper.getMembershipByReference(customer_id, function(err, membership) {
       if(err) { return callback(err, null); }
       if(!membership) { return callback(new Error("Calf not found"), null) }
 
-      var stripe_api_key = membership.account.stripe_connect.access_token;
+      ChargeHelper.getCharge(charge_id, function(err, charge) {
+        if(err) { return callback(err, null); }
 
-      //find invoice
-      StripeManager.getInvoice(stripe_api_key, invoice_id, function(err, stripe_invoice) {
-        var subscription_id = stripe_invoice.subscription;
+        ChargeHelper.parseChargeFromStripe(charge, membership, bull, stripe_charge, function(err, charge) {
+          if(err) { return callback(err, charge); }
 
-        SubscriptionHelper.getSubscription(subscription_id, function(err, subscription) {
-          if(err) { return callback(err, null); }
-          if(!subscription) { return callback(new Error("Subscription not found"), null) }
+          var message_calf = "Your charge of " + payment_total_formatted + " was updated.";
+          var message_bull= "A charge of " + payment_total_formatted + " from " + membership.user.email_address + " was updated.";
 
-          var message_calf = "Your payment of $4.00 for " + subscription.plan.name + " was successfully processed.";
-          var message_bull= "You received a payment of $4.00 for " + subscription.plan.name + " from " + membership.user.email_address + ".";
-
-          StripeEventHelper.notifyUsers("charge_succeeded", bull, membership.user, subscription.plan, message_bull, message_calf, source, received_at, function(err, activities) {
+          StripeEventHelper.notifyUsers("charge_updated", bull, membership.user, null, message_bull, message_calf, source, received_at, function(err, activities) {
             callback(err, user);
           });
-        });
+        })
       });
     });
   }
